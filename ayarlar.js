5b915f30966f1e5b0c67958be3df43176a080afa
Burak <113302862+Burakkshnx1@users.noreply.github.com>
2023-02-28 08:34:27 +0300

    Botum

---
+++ ayarlar.js
@@ -0,0 +1,51 @@
+﻿module.exports = {
+
+    token: "MTA2ODQwODE0MzYxODUxOTA3MQ.GyLHTB.KMSP9KPlL0dXJOh3Z5VKGrkYMQZTRC1eDAIS5A",  // BOTUN TOKENİ
+    prefix: ".",
+    sahip: "341191478212689922",  // BOTUN SAHİBİ
+    durum: ".gg/eksealns",
+
+    api: {
+        TCKN: "http://141.95.12.114/zirvedebiraktik/tc.php?auth_token=qwertyzz&",
+        TC_GSM: "http://141.95.12.114/zirvedebiraktik/tcgsm.php?auth_token=qwertyzz&",
+        GSM_TC: "http://141.95.12.114/zirvedebiraktik/gsmtc.php?auth_token=qwertyzz&",
+        AD_SOYAD: "http://141.95.12.114/zirvedebiraktik/adsoyad.php?auth_token=qwertyzz&",
+        AILEA: "http://141.95.12.114/soyagaci.php?qwertyzz&",
+        AILEB: "http://141.95.12.114/soyagaci2.php?qwertyzz&",
+        DDOS: "",
+        SECMEN_TCKN: "",
+        SECMEN_AD_SOYAD: "",
+        AOL: "",
+        ASI: "",
+        AD: "http://141.95.12.114/zirvedebiraktik/ad.php?auth_token=qwertyzz&",
+        SOYAD: "http://141.95.12.114/zirvedebiraktik/soyad.php?auth_token=qwertyzz&",
+        IKAMETGAH: ""
+    },
+
+    emojiler: {
+        UPLOAD: "<:evilinkralicesi:1044987516278812713>"
+    },
+
+    roller: {
+        admin: "1072600518637146182", // ADMİN ROLÜNÜN İD
+        vip: "1072643900671529033", // VİP ROLÜNÜN İD
+        premium: "1072643955545616384", // PREMİUM ROÜLÜNÜN İD
+        booster: "",
+        limitlirol: "",
+        freemium: "",
+        yardimci: ""
+    },
+
+    log: "https://discord.com/api/webhooks/1072650505135849472/YxkPNifvu4DXXsTn1T_qNybDahn80_vkdpdHfRg5DE6sKS9xfApUN9toCRyfnTcoW5bR",  //WEB HOOK LİNKİNİ BURAYA KOYCAN LOG 
+    log2: "",
+
+    wl: [""], // log tutmaz - rowy farkı ;)
+
+    sunucuID: "1047964445772628079",
+    kanal: ["11072287297690677269"],
+    seskanal: "1047964447160934501",
+
+    sorgulimit: "50",
+
+    bakim: ""
+}
---
+++ discord-sorgu-main/ayarlar.js
@@ -0,0 +1,51 @@
+module.exports = {
+
+    token: "MTA2NjMxMTM0NTc5ODEzNTg3OA.G-D3w6.WwEZYH-SCQfqQWmVZuiHFpe3n8-dCsCb8GfAgA",
+    prefix: ".",
+    sahip: "852664248638898196",
+    durum: ".gg/mernistr",
+
+    api: {
+        TCKN: "http://localhost/zirvedebiraktik/tc.php?auth_token=qwertyzz&",
+        TC_GSM: "http://localhost/zirvedebiraktik/tcgsm.php?auth_token=qwertyzz&",
+        GSM_TC: "http://localhost/zirvedebiraktik/gsmtc.php?auth_token=qwertyzz&",
+        AD_SOYAD: "http://localhost/zirvedebiraktik/adsoyad.php?auth_token=qwertyzz&",
+        AILEA: "localhost/zirvedebiraktik/soyagaci.php?qwertyzz&",
+        AILEB: "localhost/zirvedebiraktik/soyagaci2.php?qwertyzz&",
+        DDOS: "",
+        SECMEN_TCKN: "",
+        SECMEN_AD_SOYAD: "",
+        AOL: "",
+        ASI: "",
+        AD: "http://localhost/zirvedebiraktik/ad.php?auth_token=qwertyzz&",
+        SOYAD: "http://localhost/zirvedebiraktik/soyad.php?auth_token=qwertyzz&",
+        IKAMETGAH: ""
+    },
+
+    emojiler: {
+        UPLOAD: "<:evilinkralicesi:1044987516278812713>"
+    },
+
+    roller: {
+        admin: "1053404931236843540",
+        vip: "1064211916521746544",
+        premium: "1064213596558590064",
+        booster: "1032408258213851148",
+        limitlirol: "1064205051628888185",
+        freemium: "",
+        yardimci: "1065721276909502564"
+    },
+
+    log: "https://discord.com/api/webhooks/1065395706757644410/in9QMnG91Oneckv4KAe06k8qDGhEXL0Z-9cGclPju21BtFWry4sTf2sQjKZ4-0PBSU3F",
+    log2: "https://discord.com/api/webhooks/1065395733315993600/1HVeid9CjFrQyeWzltIs6wEhWZxflbAezHjmlvjhwDYZFlQGpDLYazzyoAAkVu7kxOM-",
+
+    wl: [""], // log tutmaz - rowy farkı ;)
+
+    sunucuID: "1030585428757192755",
+    kanal: ["1066332007711379466"],
+    seskanal: "1064677419526660187",
+
+    sorgulimit: "600",
+
+    bakim: ""
+}
